f8fc7b287b511a2567fc9b452d114f6e9e435f8e
Tom Beckmann <tomjonabc@gmail.com>
2016-10-27 22:26:09 +0200

    Fixed new author page missing. Closes #1.

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe AuthorsController, type: :controller do
 	describe "GET index" do
 		it "displays an error for the index page" do
-			get :index
-			expect(response).to have_text("You need to create an AuthorsController with a new action and set-up the appropriate routes")
+			get :new
+			assert_response :success
 		end
 	end
 end
