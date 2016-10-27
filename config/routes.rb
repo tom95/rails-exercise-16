296fa69d07df95bb49d55b9f826336ca794c2912
Tom Beckmann <tomjonabc@gmail.com>
2016-10-27 22:41:44 +0200

    Fix path to new_author. Closes #2.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  get 'author/new' => 'authors#new'
+  get 'new_author' => 'authors#new'
  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
