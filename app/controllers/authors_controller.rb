f23c0458b133de09e215210836f176588a7b082d
Tom Beckmann <tomjonabc@gmail.com>
2016-11-06 20:09:30 +0100

    Add create logic to author controller. Fixes #12.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,18 @@
 class AuthorsController < ApplicationController
 	def new
 	end
+
+	def create
+		@author = Author.new(author_params)
+		@author.save()
+		redirect_to @author
+	end
+
+	def show
+	end
+
+	private
+		def author_params
+			params.require(:author).permit(:first_name, :last_name, :homepage)
+		end
 end
