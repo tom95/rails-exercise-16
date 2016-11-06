1fda80708d6b0dd6f7ac5ebf57a6e93b668e3e9e
Tom Beckmann <tomjonabc@gmail.com>
2016-11-06 20:02:47 +0100

    Failing test for #12.

--- spec/features/new_author_spec.rb
+++ spec/features/new_author_spec.rb
@@ -12,4 +12,15 @@ describe "New author page", type: :feature do
     visit new_author_path
     expect(page).to have_button("Create Author")
   end
+
+  it "should save the author" do
+    visit new_author_path
+    fill_in "author_first_name", with: "Alan"
+    fill_in "author_last_name", with: "Turing"
+    fill_in "author_homepage", with: "http://wikipedia.org/Alan_Turing"
+    click_button "Create Author"
+
+    author = Author.find_by(first_name: "Alan", last_name: "Turing")
+    expect(author).to_not be_nil
+  end
 end
