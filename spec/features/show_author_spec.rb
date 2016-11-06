469cef3d2b8141c04a879c8c0222bbf0337704d1
Tom Beckmann <tomjonabc@gmail.com>
2016-11-06 21:24:57 +0100

    And again fix own test

--- spec/features/show_author_spec.rb
+++ spec/features/show_author_spec.rb
@@ -4,6 +4,6 @@ describe "Show author page", type: :feature do
   it "should display author details" do
     author = create(:author)
     visit author_path(author)
-    expect(page).to have_text("First Name: Alan")
+    expect(page).to have_text("First name: Alan")
   end
 end
