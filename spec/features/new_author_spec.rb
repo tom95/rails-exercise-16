ea8ed2564ab6bd1a29e524d7aca5fad67776a6ee
Tom Beckmann <tomjonabc@gmail.com>
2016-11-06 10:21:21 +0100

    Add last name and homepage fields. Fixes #7

--- spec/features/new_author_spec.rb
+++ spec/features/new_author_spec.rb
@@ -4,5 +4,7 @@ describe "New author page", type: :feature do
   it "should contain a field 'first name'" do
     visit new_author_path
     expect(page).to have_field("first name")
+    expect(page).to have_field("last name")
+    expect(page).to have_field("homepage")
   end
 end
