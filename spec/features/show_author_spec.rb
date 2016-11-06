8472dffa97cee813af220b776b0be41881996038
Tom Beckmann <tomjonabc@gmail.com>
2016-11-06 20:30:25 +0100

    Downcase name on detail page to satisfy the task (WTF?). Fixes #14.

--- spec/features/show_author_spec.rb
+++ spec/features/show_author_spec.rb
@@ -4,6 +4,6 @@ describe "Show author page", type: :feature do
   it "should display author details" do
     author = create(:author)
     visit author_path(author)
-    expect(page).to have_text("first name: Alan")
+    expect(page).to have_text("first name: alan")
   end
 end
