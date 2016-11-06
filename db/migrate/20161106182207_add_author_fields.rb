fa5c6e34c32b3e724ac5bfb98b53b68750cfe6b4
Tom Beckmann <tomjonabc@gmail.com>
2016-11-06 19:36:05 +0100

    Add author model with first/last_name and homepaeg. Fixes #10.

---
+++ db/migrate/20161106182207_add_author_fields.rb
@@ -0,0 +1,4 @@
+class AddAuthorFields < ActiveRecord::Migration
+  def change
+  end
+end
--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :author do
-#    first_name "Alan"
-#    last_name "Turing"
-#    homepage "http://wikipedia.de/Alan_Turing"
+    first_name "MyString"
+last_name "MyString"
+homepage "MyString"
   end
+
 end
