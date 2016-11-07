26f5eb2c94a304cda78520636dc8619cd2dd8e99
Tom Beckmann <tomjonabc@gmail.com>
2016-11-07 22:00:08 +0100

    Add edit author page. Fixes #26.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -17,6 +17,16 @@ class AuthorsController < ApplicationController
 	end
 
 	def edit
+		@author = Author.find(params[:id])
+	end
+
+	def update
+		@author = Author.find(params[:id])
+		if @author.update(author_params)
+			redirect_to @author
+		else
+			render :edit
+		end
 	end
 
 	def show
