a5a738db1d68c1161dc4f22b27cbaf6b1c5f5fb6
Tom Beckmann <tomjonabc@gmail.com>
2016-11-07 18:07:12 +0100

    Failing test for #22.

--- spec/features/index_author_spec.rb
+++ spec/features/index_author_spec.rb
@@ -12,13 +12,19 @@ describe "Index author page", type: :feature do
     expect(page).to have_text("http://wikipedia.org/Alan_Turing")
   end
 
-  it "list first and last name in one column" do
+  it "should list first and last name in one column" do
     visit authors_path
     expect(page).to have_css("th", text: "Name")
   end
 
-  it "link to the new author page" do
+  it "should link to the new author page" do
     visit authors_path
     expect(page).to have_css("a", text: "Add author")
+  end
+
+  it "should link to the author page" do
+    create(:author)
+    visit authors_path
+    expect(page).to have_css("a", text: "Show")
   end
 end
