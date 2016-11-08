a54f34e9a177e097bff69f865ba5cc439cb43dfb
Tom Beckmann <tomjonabc@gmail.com>
2016-11-09 00:39:27 +0100

    Failing test for #39

--- spec/features/edit_paper_spec.rb
+++ spec/features/edit_paper_spec.rb
@@ -5,5 +5,17 @@ describe "Edit paper page", type: :feature do
     paper = create(:paper)
     visit edit_paper_path(paper)
   end
+
+  it "should save changes" do
+    paper = create(:paper)
+
+    visit edit_paper_path(paper)
+    fill_in "paper_venue", with: "Mind 49"
+    click_button "Save Paper"
+
+    new_paper = Paper.find_by(venue: "Mind 49")
+    expect(new_paper).to_not be_nil
+    expect(new_paper.title).to eq(paper.title)
+  end
 end
 
