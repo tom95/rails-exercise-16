a8830f2a7e1f62d7057281138d106f9848d15ce7
Tom Beckmann <tomjonabc@gmail.com>
2016-11-09 00:25:15 +0100

    Failing test for #37

--- spec/features/index_paper_spec.rb
+++ spec/features/index_paper_spec.rb
@@ -4,5 +4,11 @@ describe "Index paper page", type: :feature do
   it "should render" do
     visit papers_path
   end
+
+  it "should list title, venue, and year of all papers" do
+    create(:paper)
+    visit papers_path
+    expect(page).to have_text("COMPUTING MACHINERY AND INTELLIGENCE")
+  end
 end
 
