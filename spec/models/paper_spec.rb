f4c45dcdfb3a866b8ba31e09e5cfa7dded23fadf
Tom Beckmann <tomjonabc@gmail.com>
2016-11-08 23:26:54 +0100

    Failig test for #31

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
+  it "should have title, venue, and year" do
+    paper = build(:paper)
+    expect(paper.year).to eq(1950)
+  end
 end
