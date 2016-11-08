eba6979e3f6f1ba674de9ed83fea97b83b66fc1b
Tom Beckmann <tomjonabc@gmail.com>
2016-11-08 23:55:08 +0100

    Require paper's year to be integer. Fixes #34

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,5 @@
 class Paper < ActiveRecord::Base
   validates :title, presence: true
   validates :venue, presence: true
-  validates :year, presence: true
+  validates :year, presence: true, numericality: { only_integer: true  }
 end
