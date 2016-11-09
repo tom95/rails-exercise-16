8c05ccd314d55380f31bab56a6805773a84b761b
Tom Beckmann <tomjonabc@gmail.com>
2016-11-09 15:58:14 +0100

    Failing test for #47

--- spec/features/index_paper_spec.rb
+++ spec/features/index_paper_spec.rb
@@ -18,5 +18,12 @@ describe "Index paper page", type: :feature do
     click_link "Destroy"
     expect(Paper.find_by title: "COMPUTING MACHINERY AND INTELLIGENCE").to be_nil
   end
+
+  it "should allow to filter by year" do
+    create(:paper)
+    create(:paper, title: "go to statement considered harmful", venue: "author", year: 1968)
+    visit papers_path(year: 1950)
+    expect(page).not_to have_text("go to statement considered harmful")
+  end
 end
 
