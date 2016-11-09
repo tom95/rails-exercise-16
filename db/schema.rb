e6132aa3a61bad40fd498da01379b40e57a3fcad
Tom Beckmann <tomjonabc@gmail.com>
2016-11-09 10:33:01 +0100

    Add dropdown for selecting authors. Fixes #43

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20161109085708) do
   create_table "papers", force: :cascade do |t|
     t.string   "title"
     t.string   "venue"
-    t.integer  "year"
+    t.string   "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
