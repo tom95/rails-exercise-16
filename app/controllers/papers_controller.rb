7ec0aa21e4839293f1e5dcae4d1a41a5f734ca4f
Tom Beckmann <tomjonabc@gmail.com>
2016-11-09 16:01:53 +0100

    Show only papers from year. Fixes #47

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,6 +1,10 @@
 class PapersController < ApplicationController
   def index
-    @papers = Paper.all
+    if params[:year]
+      @papers = Paper.where("year = ?", params[:year])
+    else
+      @papers = Paper.all
+    end
   end
 
   def new
